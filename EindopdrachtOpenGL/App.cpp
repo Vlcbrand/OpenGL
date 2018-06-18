2c2a41c6e6b3f964e3fd5581fcb487db739544c2
Vlcbrand <Vlcbrand@avans.nl>
2018-06-18 10:32:58 +0200

    initial commit

--- EindopdrachtOpenGL/App.cpp
+++ EindopdrachtOpenGL/App.cpp
@@ -1,51 +1,61 @@
 #pragma once
-#include <iostream>
-
 #define GLEW_STATIC
 #include "glew.h"
 #include "glfw3.h"
-
+#include <iostream>
 #include "Utility.h"
 #include "KeyHandler.h"
 
 using namespace std;
 
-int main() {
+//set parameter to true for a fullscreen application
+static bool FULLSCREEN = false;
+GLFWwindow* gWindow = nullptr;
 
+bool initOpenGL() {
 	if (!glfwInit()) {
 		cerr << "GLFW cant init" << endl;
-		return -1;
+		return false;
 	}
-	
-	// SET PARAMETER TO TRUE FOR FULL SCREEN
-	GLFWwindow *pWindow = InitWindow(false);
-	if (pWindow == nullptr) {
+
+	gWindow = InitWindow(FULLSCREEN);
+
+	if (gWindow == nullptr) {
 		cerr << "Window cant init" << endl;
 		glfwTerminate();
-		return -1;
+		return false;
 	}
-	glfwMakeContextCurrent(pWindow);
+	glfwMakeContextCurrent(gWindow);
 
-	glfwSetKeyCallback(pWindow, OnKey);
+	glfwSetKeyCallback(gWindow, OnKey);
 
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK) {
 		cerr << "GLEW cant init" << endl;
 		glfwTerminate();
+		return false;
+	}
+
+	glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
+	return true;
+}
+
+int main() {
+
+	if (!initOpenGL()) {
 		return -1;
 	}
 
 	//main loop
-	while (!glfwWindowShouldClose(pWindow)) {
-		showFPS(pWindow);
+	while (!glfwWindowShouldClose(gWindow)) {
+		showFPS(gWindow);
 
 		//polls for events like keypresses
 		glfwPollEvents();
 
-		glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glfwSwapBuffers(pWindow);
+		glfwSwapBuffers(gWindow);
 	}
 
 	glfwTerminate();
